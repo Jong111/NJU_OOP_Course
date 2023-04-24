1a482e501ec45b5fb5193d5b157a72071151cb43
HeJunting <1401607862@qq.com>
2023-04-24 22:01:51 +0800

    完成了7.md中的作业

--- ProjectCourse2/test1.cpp
+++ ProjectCourse2/test1.cpp
@@ -1,47 +1,47 @@
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-class Time {
-public:
-	int h;
-	int m;
-	int s;
-	
-	Time(int h, int m, int s) {
-		this->h = h;
-		this->m = m;
-		this->s = s;
-	}
-
-	void set(int h, int m, int s) {
-		this->h = h;
-		this->m = m;
-		this->s = s;
-	}
-
-	void display() {
-		cout << h << "h " << m << "m " << s << "s " << endl;
-	}
-
-	bool equal(Time& other_time) {
-		if (other_time.h == this->h && other_time.m == this->m && other_time.s == this->s) {
-			return true;
-		}
-		return false;
-	}
-};
-
-int main() {
-	Time time(23, 56, 7);
-	time.display();
-	time.set(22, 34, 59);
-	time.display();
-	Time time2(22, 35, 36);
-	Time time3(22, 34, 59);
-	cout << time.equal(time2) << endl;
-	cout << time.equal(time3) << endl;
-	return 0;
-}
-
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//class Time {
+//public:
+//	int h;
+//	int m;
+//	int s;
+//	
+//	Time(int h, int m, int s) {
+//		this->h = h;
+//		this->m = m;
+//		this->s = s;
+//	}
+//
+//	void set(int h, int m, int s) {
+//		this->h = h;
+//		this->m = m;
+//		this->s = s;
+//	}
+//
+//	void display() {
+//		cout << h << "h " << m << "m " << s << "s " << endl;
+//	}
+//
+//	bool equal(Time& other_time) {
+//		if (other_time.h == this->h && other_time.m == this->m && other_time.s == this->s) {
+//			return true;
+//		}
+//		return false;
+//	}
+//};
+//
+//int main() {
+//	Time time(23, 56, 7);
+//	time.display();
+//	time.set(22, 34, 59);
+//	time.display();
+//	Time time2(22, 35, 36);
+//	Time time3(22, 34, 59);
+//	cout << time.equal(time2) << endl;
+//	cout << time.equal(time3) << endl;
+//	return 0;
+//}
+//
--- ProjectCourse2/test3.cpp
+++ ProjectCourse2/test3.cpp
@@ -10,7 +10,7 @@
 //
 //    // 判断子串是否在当前字符串里
 //    bool is_substring(const String& sub_str) {
-//        if (this->find(sub_str) != string::npos) { // 如果找到子串
+//        if (string::find(sub_str) != string::npos) { // 如果找到子串
 //            return true; // 返回true
 //        }
 //        else { // 如果没找到子串
@@ -23,10 +23,43 @@
 //        int count = 0; // 记录替换次数
 //        size_t pos = this->find(find); // 查找第一个子串的位置
 //        while (pos != string::npos) { // 如果找到子串
-//            this->replace(pos, strlen(find), replace); // 替换子串
+//            string::replace(pos, strlen(find), replace); // 替换子串
 //            count++; // 增加替换次数
 //            pos = this->find(find, pos + strlen(replace)); // 查找下一个子串的位置
 //        }
 //        return count; // 返回替换次数
 //    }
+//
 //};
+//
+//int main() {
+//    String s1("NJU CS");
+//    cout << s1 << endl;
+//
+//    String s2(s1);
+//    cout << s2 << endl;
+//
+//    String s3("NJU OOP");
+//    cout << s3 << endl;
+//
+//    s3 = s1;
+//    cout << endl;
+//
+//
+//    String s5("NJU");
+//    cout << s1.is_substring(s5) << endl;
+//
+//    String s6("");
+//    cout << s1.is_substring(s6) << endl;
+//
+//    cout << s1.replace("NJU", "THU") << endl;
+//    cout << s1 << endl;
+//
+//    cout << s2.replace("J", "K") << endl;
+//    cout << s2;
+//
+//    cout << s3.replace("NJU", "Stanford") << endl;
+//    // s3.print();
+//
+//    return 0;
+//}
