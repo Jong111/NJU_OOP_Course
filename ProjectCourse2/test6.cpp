2f68f34133478848d6447c6138e929007e75be89
HeJunting <1401607862@qq.com>
2023-05-05 16:45:51 +0800

    期中考试答案

---
+++ ProjectCourse2/test6.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main()
+//{
+//    vector<int> input;
+//    int number;
+//    while (cin >> number)
+//    {
+//        input.push_back(number);
+//        if (cin.get() == '\n') //按下回车键推出循环
+//            break;
+//    }
+//    vector<int> res;
+//    for (int i = 0; i <= number - 3; i++) {
+//        int tmp = input[i] + input[i + 1] + input[i + 2];
+//        res.push_back(tmp);
+//    }
+//    for (int i = 0; i < res.size(); i++) {
+//        cout << res[i] << " ";
+//    }
+//    return 0;
+//}
