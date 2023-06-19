b9fe6758ceb598dca86a7ce860acf93c4c7a72fe
HeJunting <1401607862@qq.com>
2023-06-19 21:05:24 +0800

    完成了13.md中的作业

--- ProjectCourse2/test3.cpp
+++ ProjectCourse2/test3.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-using namespace std;
-
-// 基类
-class Base {
-public:
-    virtual void func() { // 非虚函数
-        cout << "Base::func()" << endl;
-    }
-};
-
-// 派生类
-class Derived : public Base {
-public:
-    void func() { // 隐藏基类的函数
-        cout << "Derived::func()" << endl;
-    }
-};
-
-// 测试代码
-int main() {
-    Derived d;
-    Base* p = &d; // 基类指针指向派生类对象
-    p->func(); // 调用 Base::func()
-    return 0;
-}
-
-
-
-
-
-
+//#include <iostream>
+//using namespace std;
+//
+//// 基类
+//class Base {
+//public:
+//    virtual void func() { // 非虚函数
+//        cout << "Base::func()" << endl;
+//    }
+//};
+//
+//// 派生类
+//class Derived : public Base {
+//public:
+//    void func() { // 隐藏基类的函数
+//        cout << "Derived::func()" << endl;
+//    }
+//};
+//
+//// 测试代码
+//int main() {
+//    Derived d;
+//    Base* p = &d; // 基类指针指向派生类对象
+//    p->func(); // 调用 Base::func()
+//    return 0;
+//}
+//
+//
+//
+//
+//
+//
